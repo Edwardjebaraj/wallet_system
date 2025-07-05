7164c97f38a339d5eff56a3320e2a7498c4d04bc
Edwardjebaraj <51898770+Edwardjebaraj@users.noreply.github.com>
2025-07-06 01:33:33 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import { App } from './app'; 
-
+import { App } from './app';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const server = new App(); // port is not needed on Vercel
 
-async function main() {
-    const EPIC = await process.env.PORT;
-    const app = new App(EPIC); 
-    await app.listen();
-}
+const expressApp = (async () => {
+  await server.settings(); // make sure middlewares and routes are ready
+  return server['app'];    // expose internal express Application
+})();
 
-main();
+module.exports = async (req: any, res: any) => {
+  const app = await expressApp;
+  return app(req, res); // let Vercel handle it
+};
